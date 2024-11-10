097f13814816fe8a7d416afa5717c3e633343487
Ihor Ivanets <ivanets22@gmail.com>
2024-11-10 17:11:01 +0200

    completed task

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,11 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from "..";
+
+export const PhotosGallery = ({ images }) => {
+  return (
+    <Grid>
+      {images.map(({ alt, avg_color, id, src }) => (
+        <PhotosGalleryItem key={id} alt={alt} avg_color={avg_color} src={src} />
+      ))}
+    </Grid>
+  );
 };
--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,3 +1,15 @@
-export const PhotosGalleryItem = () => {
-  return <h3>PhotosGalleryItem</h3>;
+import { GridItem } from "..";
+import s from "./PhotosGalleryItem.module.css";
+
+export const PhotosGalleryItem = ({ alt, avg_color, src }) => {
+  return (
+    <GridItem>
+      <div
+        style={{ backgroundColor: avg_color, borderColor: avg_color }}
+        className={s.thumb}
+      >
+        <img src={src.large} alt={alt} />
+      </div>
+    </GridItem>
+  );
 };
